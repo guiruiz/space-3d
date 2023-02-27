4e528612c189a8f58e4f1b936976b2fd937584a8
Guilherme Ruiz <guiiruiz@hotmail.com>
2023-02-27 22:24:07 +1100

    Initial commit

---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+    int yawSpeed = 40;
+    int pitchSpeed = 40;
+    int rollSpeed = 40;
+
+    private float throttle = 0;
+    private float throttleSensitivity = .2f;
+    private float maxThrust = 20f;
+    private float maxSpeed = 30f;
+    void Start()
+    {
+        rigidBody = this.GetComponent<Rigidbody>();
+
+        //Set ship in orbit at alt 97m
+        rigidBody.AddForce(transform.TransformDirection(Vector3.left) * 1500f);
+
+    }
+
+    void Update()
+    {
+
+        if (rigidBody.velocity.magnitude > maxSpeed)
+        {
+            rigidBody.velocity = Vector3.ClampMagnitude(rigidBody.velocity, maxSpeed);
+        }
+
+    }
+    void FixedUpdate()
+    {
+        FlightControl();
+        ThrottleControl();
+        ThrustControl();
+    }
+
+    void FlightControl()
+    {
+        // ROLL
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.forward * yawSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward * yawSpeed * Time.deltaTime);
+        }
+
+        // PITCH
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(-Vector3.right * pitchSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(Vector3.right * pitchSpeed * Time.deltaTime);
+        }
+
+        // YAW
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(-Vector3.up * rollSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.up * rollSpeed * Time.deltaTime);
+        }
+    }
+
+    void ThrottleControl()
+    {
+        float t = throttle;
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            t = 0;
+        }
+        else if (Input.GetKey(KeyCode.Z))
+        {
+            t = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            t = throttle + Time.deltaTime * throttleSensitivity;
+        }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            t = throttle - Time.deltaTime * throttleSensitivity;
+        }
+
+        t = Mathf.Clamp(t, 0, 1f);
+
+
+        throttle = t;
+    }
+
+    void ThrustControl()
+    {
+        if (throttle > 0.02f)
+        {
+            rigidBody.AddForce(transform.TransformDirection(Vector3.up) * maxThrust * throttle);
+        }
+    }
+
+    public float getThrottle()
+    {
+        return throttle;
+    }
+}
---
+++ Assets/UI/RocketDataUI.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketDataUI : MonoBehaviour
+{
+    public GameObject rocket;
+    public GameObject planet;
+    public TMPro.TMP_Text throttleTextUI;
+    public TMPro.TMP_Text altituteTextUI;
+    public TMPro.TMP_Text velocityTextUI;
+    public TMPro.TMP_Text speedTextUI;
+    public TMPro.TMP_Text pitchTextUI;
+    public TMPro.TMP_Text rollTextUI;
+    public TMPro.TMP_Text yawTextUI;
+
+    int altitudeOffset = 104; // @todo Calculate offset dinamicaly
+    private float time;
+    private float altitude = 0;
+    private float lastAltitudeM = 0;
+    float velocityRate = 10f;
+
+    void Start()
+    {
+        time = Time.time;
+    }
+
+    void FixedUpdate()
+    {
+        UpdateThrottle();
+        UpdateAltitude();
+        UpdateVelocity();
+        UpdateSpeed();
+        UpdateRotation();
+    }
+
+
+    void UpdateThrottle()
+    {
+        RocketController rocketController = rocket.GetComponent<RocketController>();
+        int throttlePercentage = (int)(rocketController.getThrottle() * 100);
+
+        throttleTextUI.text = "Throttle: " + throttlePercentage + "%";
+    }
+    void UpdateRotation()
+    {
+        Vector3 rotation = rocket.transform.rotation.eulerAngles;
+
+        pitchTextUI.text = "Pitch: " + ((int)rotation.x) + "º";
+        rollTextUI.text = "Roll: " + ((int)rotation.z) + "º";
+        yawTextUI.text = "Yaw: " + ((int)rotation.y) + "º";
+    }
+
+    void UpdateAltitude()
+    {
+        Rigidbody rb1 = rocket.GetComponent<Rigidbody>();
+        Rigidbody rb2 = planet.GetComponent<Rigidbody>();
+
+
+        Vector3 direction = rb1.position - rb2.position;
+        float distance = direction.magnitude;
+        ;
+
+        float alt = distance - altitudeOffset;
+        alt = Mathf.Max(alt, 0);
+        altitude = alt;
+
+        if (alt < 1000)
+        {
+            alt = (int)alt;
+            altituteTextUI.text = "Alt: " + alt + "m";
+        }
+        else
+        {
+            alt = alt / 1000;
+            alt = (float)Mathf.Round(alt * 100f) / 100f;
+            altituteTextUI.text = "Alt: " + alt.ToString("F2") + "km";
+        }
+
+    }
+
+    void UpdateSpeed()
+    {
+
+        Rigidbody rb = rocket.GetComponent<Rigidbody>();
+
+        speedTextUI.text = "Speed: " + rb.velocity.magnitude.ToString("F2") + "m/s";
+    }
+
+    void UpdateVelocity()
+    {
+
+        if (Time.time >= time + (1f / velocityRate))
+        {
+            float velocity = (altitude - lastAltitudeM) * velocityRate;
+
+            velocityTextUI.text = "Δ Vel.: " + velocity.ToString("F2") + "m/s";
+
+            time = Time.time;
+            lastAltitudeM = altitude;
+        }
+    }
+}
