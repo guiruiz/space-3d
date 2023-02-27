9d840fdde80ae1803812fe935415a3b52fcc5c01
Guilherme Ruiz <guiiruiz@hotmail.com>
2023-02-27 22:29:15 +1100

    Lint

--- Assets/Scripts/PlanetGravity.cs
+++ Assets/Scripts/PlanetGravity.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class PlanetGravity : MonoBehaviour
 {
-    private float G = 30000;
-    public GameObject objectToAttract;
+  private float G = 30000;
+  public GameObject objectToAttract;
 
-    Rigidbody rb;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+  Rigidbody rb;
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
 
-    void FixedUpdate()
-    {
-        Attract();
-    }
+  void FixedUpdate()
+  {
+    Attract();
+  }
 
-    void Attract()
-    {
-        Rigidbody rbToAttract = objectToAttract.GetComponent<Rigidbody>();
+  void Attract()
+  {
+    Rigidbody rbToAttract = objectToAttract.GetComponent<Rigidbody>();
 
-        Vector3 direction = rb.position - rbToAttract.position;
-        float distance = direction.magnitude;
+    Vector3 direction = rb.position - rbToAttract.position;
+    float distance = direction.magnitude;
 
-        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
-        Vector3 force = direction.normalized * forceMagnitude;
+    float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+    Vector3 force = direction.normalized * forceMagnitude;
 
-        rbToAttract.AddForce(force);
-    }
+    rbToAttract.AddForce(force);
+  }
 }
--- Assets/Scripts/RocketController.cs
+++ Assets/Scripts/RocketController.cs
@@ -5,111 +5,111 @@ using UnityEngine;
 
 public class RocketController : MonoBehaviour
 {
-    private Rigidbody rigidBody;
+  private Rigidbody rigidBody;
 
-    int yawSpeed = 40;
-    int pitchSpeed = 40;
-    int rollSpeed = 40;
+  int yawSpeed = 40;
+  int pitchSpeed = 40;
+  int rollSpeed = 40;
 
-    private float throttle = 0;
-    private float throttleSensitivity = .2f;
-    private float maxThrust = 20f;
-    private float maxSpeed = 30f;
-    void Start()
+  private float throttle = 0;
+  private float throttleSensitivity = .2f;
+  private float maxThrust = 20f;
+  private float maxSpeed = 30f;
+  void Start()
+  {
+    rigidBody = this.GetComponent<Rigidbody>();
+
+    //Set ship in orbit at alt 97m
+    rigidBody.AddForce(transform.TransformDirection(Vector3.left) * 1500f);
+
+  }
+
+  void Update()
+  {
+
+    if (rigidBody.velocity.magnitude > maxSpeed)
     {
-        rigidBody = this.GetComponent<Rigidbody>();
-
-        //Set ship in orbit at alt 97m
-        rigidBody.AddForce(transform.TransformDirection(Vector3.left) * 1500f);
-
+      rigidBody.velocity = Vector3.ClampMagnitude(rigidBody.velocity, maxSpeed);
     }
 
-    void Update()
+  }
+  void FixedUpdate()
+  {
+    FlightControl();
+    ThrottleControl();
+    ThrustControl();
+  }
+
+  void FlightControl()
+  {
+    // ROLL
+    if (Input.GetKey(KeyCode.A))
     {
-
-        if (rigidBody.velocity.magnitude > maxSpeed)
-        {
-            rigidBody.velocity = Vector3.ClampMagnitude(rigidBody.velocity, maxSpeed);
-        }
-
+      transform.Rotate(-Vector3.forward * yawSpeed * Time.deltaTime);
     }
-    void FixedUpdate()
+    else if (Input.GetKey(KeyCode.D))
     {
-        FlightControl();
-        ThrottleControl();
-        ThrustControl();
-    }
-
-    void FlightControl()
-    {
-        // ROLL
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(-Vector3.forward * yawSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(Vector3.forward * yawSpeed * Time.deltaTime);
-        }
-
-        // PITCH
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Rotate(-Vector3.right * pitchSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.Rotate(Vector3.right * pitchSpeed * Time.deltaTime);
-        }
-
-        // YAW
-        if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(-Vector3.up * rollSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(Vector3.up * rollSpeed * Time.deltaTime);
-        }
+      transform.Rotate(Vector3.forward * yawSpeed * Time.deltaTime);
     }
 
-    void ThrottleControl()
+    // PITCH
+    if (Input.GetKey(KeyCode.W))
     {
-        float t = throttle;
-
-        if (Input.GetKey(KeyCode.X))
-        {
-            t = 0;
-        }
-        else if (Input.GetKey(KeyCode.Z))
-        {
-            t = 1;
-        }
-        else if (Input.GetKey(KeyCode.LeftShift))
-        {
-            t = throttle + Time.deltaTime * throttleSensitivity;
-        }
-        else if (Input.GetKey(KeyCode.LeftControl))
-        {
-            t = throttle - Time.deltaTime * throttleSensitivity;
-        }
-
-        t = Mathf.Clamp(t, 0, 1f);
-
-
-        throttle = t;
+      transform.Rotate(-Vector3.right * pitchSpeed * Time.deltaTime);
+    }
+    else if (Input.GetKey(KeyCode.S))
+    {
+      transform.Rotate(Vector3.right * pitchSpeed * Time.deltaTime);
     }
 
-    void ThrustControl()
+    // YAW
+    if (Input.GetKey(KeyCode.Q))
     {
-        if (throttle > 0.02f)
-        {
-            rigidBody.AddForce(transform.TransformDirection(Vector3.up) * maxThrust * throttle);
-        }
+      transform.Rotate(-Vector3.up * rollSpeed * Time.deltaTime);
+    }
+    else if (Input.GetKey(KeyCode.E))
+    {
+      transform.Rotate(Vector3.up * rollSpeed * Time.deltaTime);
+    }
+  }
+
+  void ThrottleControl()
+  {
+    float t = throttle;
+
+    if (Input.GetKey(KeyCode.X))
+    {
+      t = 0;
+    }
+    else if (Input.GetKey(KeyCode.Z))
+    {
+      t = 1;
+    }
+    else if (Input.GetKey(KeyCode.LeftShift))
+    {
+      t = throttle + Time.deltaTime * throttleSensitivity;
+    }
+    else if (Input.GetKey(KeyCode.LeftControl))
+    {
+      t = throttle - Time.deltaTime * throttleSensitivity;
     }
 
-    public float getThrottle()
+    t = Mathf.Clamp(t, 0, 1f);
+
+
+    throttle = t;
+  }
+
+  void ThrustControl()
+  {
+    if (throttle > 0.02f)
     {
-        return throttle;
+      rigidBody.AddForce(transform.TransformDirection(Vector3.up) * maxThrust * throttle);
     }
+  }
+
+  public float getThrottle()
+  {
+    return throttle;
+  }
 }
